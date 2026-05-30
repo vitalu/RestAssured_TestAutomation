a54f8987b36d983e4aec7f6e93d2481369809170
Vital <vital.kumar77.com>
2026-05-30 12:44:56 +0530

    Created config manager class for multiple envrionments (dev,qa,uat)

--- src/test/java/com/api/tests/LoginAPITest.java
+++ src/test/java/com/api/tests/LoginAPITest.java
@@ -3,18 +3,22 @@ package com.api.tests;
 import static io.restassured.RestAssured.*;
 import static org.hamcrest.Matchers.*;
 
+import java.io.IOException;
+
 import org.testng.annotations.Test;
 
 import com.api.pojo.UserCredentials;
+import static com.api.utils.ConfigManager.*;
 
 import io.restassured.http.ContentType;
 import io.restassured.module.jsv.JsonSchemaValidator;
 
 public class LoginAPITest {
 	@Test
-	public void loginAPITest() {
+	public void loginAPITest() throws IOException {
+		
 		UserCredentials userCredentials = new UserCredentials("iamfd", "password");
-		given().baseUri("http://64.227.160.186:9000/v1").and().contentType(ContentType.JSON).and()
+		given().baseUri(getProperty("BASE_URI")).and().contentType(ContentType.JSON).and()
 				.accept(ContentType.JSON).body(userCredentials)
 				.log().uri()
 				.log().method()
---
+++ src/test/java/com/api/utils/ConfigManager.java
@@ -0,0 +1,55 @@
+package com.api.utils;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Properties;
+
+public class ConfigManager {
+//WAP to read the properties file from src/test/resources/config/config.properties
+	private static Properties properies = new Properties();
+	private static String propertiesFilePath;
+	private static String env;
+
+	private ConfigManager() {
+
+	}
+
+	static {
+		env = System.getProperty("env");
+		env = env.toLowerCase().trim();
+		System.out.println("*** Running tests in "+env+ " environment ***");
+		switch (env) {
+		case "dev" -> propertiesFilePath = "config/config.dev.properties";
+		case "qa" -> propertiesFilePath = "config/config.qa.properties";
+		case "uat" -> propertiesFilePath = "config/config.uat.properties";
+		default -> propertiesFilePath = "config/config.properties";
+		}
+		InputStream input = Thread.currentThread().getContextClassLoader().getResourceAsStream(propertiesFilePath);
+		// load the properties file using the load()
+//		File configFile = new File(System.getProperty("user.dir") + File.separator + "src" + File.separator
+//				+ "test" + File.separator + "resources" + File.separator + "config" + File.separator
+//				+ "config.properties");
+//		FileReader fileReader = null;
+		if (propertiesFilePath == null) {
+			throw new RuntimeException("Can not find the file at the provided path " + propertiesFilePath);
+		}
+		try {
+			// fileReader = new FileReader(input);
+			properies.load(input);
+		} catch (FileNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+
+	public static String getProperty(String key) throws IOException {
+
+		return properies.getProperty("BASE_URI");
+	}
+}
