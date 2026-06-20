cafeabadffae39b21055c0fb43e182ca9aed1eb0
Vital <vital.kumar77.com>
2026-06-20 12:08:27 +0530

    Practiced mapping the CSV to POJO

---
+++ src/main/java/com/demo/csv/UserPojo.java
@@ -0,0 +1,32 @@
+package com.demo.csv;
+
+public class UserPojo {
+	private String username;
+	private String password;
+	
+	public UserPojo(){
+		
+	}
+	public UserPojo(String username, String password) {
+		super();
+		this.username = username;
+		this.password = password;
+	}
+	public String getUsername() {
+		return username;
+	}
+	public void setUsername(String username) {
+		this.username = username;
+	}
+	public String getPassword() {
+		return password;
+	}
+	public void setPassword(String password) {
+		this.password = password;
+	}
+	@Override
+	public String toString() {
+		return "UserPojo [username=" + username + ", password=" + password + "]";
+	}
+	
+}
